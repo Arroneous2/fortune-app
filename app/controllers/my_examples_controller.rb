b71e2212520ad0f9f70fd37e0085fde70cb50ae1
Arroneous2 <thumpinbassbeat@gmail.com>
2024-05-10 13:44:44 -0500

    some basic json

--- app/controllers/my_examples_controller.rb
+++ app/controllers/my_examples_controller.rb
@@ -1,2 +1,22 @@
 class MyExamplesController < ApplicationController
+  def get_fortune
+    fortune_number = rand(5) 
+    fortune_array = ["Something bad will happen to you today", "Look out for falling rocks", "Things will be looking up for you soon", "I wouldn't do that if I were you", "Your about to make it rich!"]
+
+    lotto_numbers = []
+    while lotto_numbers.length < 6
+      rand_number = rand(60) + 1
+
+      #make sure the number is unique
+      while lotto_numbers.include?(rand_number)
+        rand_number = rand(60) + 1
+      end
+
+      lotto_numbers.push(rand_number)
+    end
+    
+    render json: {fortune: "#{fortune_array[fortune_number]}",
+                  lotto_numbers: lotto_numbers
+                  }
+  end
 end
